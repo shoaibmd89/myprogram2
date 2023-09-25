0ea94f9fb7927882f4d730337f3b52238a885bb4
Shoaib Ansari <s9771995@gmail.com>
2023-09-26 01:50:57 +0530

    finding the fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,21 @@
+package myprogram2;
+import java.util.Scanner;
+class fibonacci{
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        int a=0;
+        int b=1;
+        int c;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i=1;i<=n;i++){
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+
+        }
+    }
+}
