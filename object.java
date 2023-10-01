f3c515d00feda057eab54ce9d700eac955669580
Shoaib Ansari <s9771995@gmail.com>
2023-10-01 17:45:45 +0530

    object creating

---
+++ object.java
@@ -0,0 +1,22 @@
+package myprogram2;
+ class obj{
+ 
+ static int sum(int a, int b){
+
+    int add=a+b;
+    return add;
+    } 
+}       
+public class object{
+    public static void main(String[] args) {
+      //  obj bc=new obj();
+        
+        System.out.println(obj.sum(3,4));
+    }
+}
+
+       
+
+
+
+
